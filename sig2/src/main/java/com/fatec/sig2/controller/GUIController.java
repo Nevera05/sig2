27f7c11723d0055b41e6337d529fdbf25287faf6
dti <dti@LAB104-12>
2022-03-15 10:47:36 -0300

    Versão Inicial

---
+++ sig2/src/main/java/com/fatec/sig2/controller/GUIController.java
@@ -0,0 +1,26 @@
+package com.fatec.sig2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GUIController {
+	@GetMapping("/")
+	public ModelAndView menu() {
+		return new ModelAndView("paginaMenu");
+	}
+	@GetMapping("/login")
+	public ModelAndView login() {
+		return new ModelAndView("login");
+	}
+	@GetMapping ("/clientes")
+	 public ModelAndView cadastrarCliente() {
+	 return new ModelAndView ("cadastrarCliente");
+	 }
+	 @GetMapping("/fornecedores")
+	 public ModelAndView cadastrarFornecedor() {
+	 return new ModelAndView ("cadastrarFornecedor");
+	 } 
+
+}
